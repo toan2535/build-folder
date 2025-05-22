56845eabac619bf057659f235954eaf39875fd37
toan2535 <nguyengiatoan2004@gmail.com>
2025-05-21 21:54:07 -0700

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/tiec/Desktop/lino_ws/src
+# Build directory: /home/tiec/Desktop/lino_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lino_visualize")
+subdirs("lino_msgs")
+subdirs("lino_pid")
+subdirs("linorobot")
---
+++ lino_visualize/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tiec/Desktop/lino_ws/src/lino_visualize
+# Build directory: /home/tiec/Desktop/lino_ws/build/lino_visualize
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
